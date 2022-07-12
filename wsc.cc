e436316b7c2de7a73ce6278683ca431c3668ccbc
Ezekiel Warren <zekewarren@gmail.com>
2022-07-11 23:40:26 -0700

    tabs

--- wsc.cc
+++ wsc.cc
@@ -20,12 +20,12 @@ constexpr auto WSC_SH = R"(#!/usr/bin/env bash
 set -e
 WSC_EXEC="./.wsc/wsc"
 if [ -f "$WSC_EXEC" ]; then
-  $WSC_EXEC
+	$WSC_EXEC
 else 
 	>&2 echo "🔔"
-  >&2 echo "🔔 Missing wsc executable. Workspace statuses will not be reported."
+	>&2 echo "🔔 Missing wsc executable. Workspace statuses will not be reported."
 	>&2 echo "🔔 Have you done wsc init?"
-  >&2 echo "🔔 ❯ bazel run @wsc init"
+	>&2 echo "🔔 ❯ bazel run @wsc init"
 	>&2 echo "🔔"
 fi
 )";
